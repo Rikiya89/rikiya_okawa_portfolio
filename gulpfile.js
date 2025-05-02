c655dfaf52e369fc77f21072243394081465d283
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2025-05-02 18:47:29 +0900

    new-version

--- gulpfile.js
+++ gulpfile.js
@@ -2,116 +2,109 @@ const gulp = require('gulp');
 const path = require("path");
 const glob = require('glob');
 const dartSass = require('gulp-dart-sass');
-const sassGlob = require('gulp-sass-glob-use-forward');
+const sassGlob = require( 'gulp-sass-glob-use-forward');
 const autoprefixer = require('gulp-autoprefixer');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
 const plumber = require("gulp-plumber");
+
 const browsersync = require('browser-sync').create();
 const connectSSI = require('gulp-connect-ssi');
 const connectPHP = require('gulp-connect-php');
 
 let SETTING = require('./gulpfile.setting.js');
 
-// Set correct paths for JS and CSS in the settings file or directly here if needed
-SETTING.WEBPACK.JS_SOURCE_PATH = '/Users/okawa_rikiya/Documents/Design/rikiya_okawa_portfolio/assets/js';  // Example path
-SETTING.SASS.SOURCE_PATH = '/Users/okawa_rikiya/Documents/Design/rikiya_okawa_portfolio/assets/sass';  // Example path
 
 gulp.task('sass', (done) => {
-    gulp.src(path.join(SETTING.SASS.SOURCE_PATH, SETTING.SASS.SOURCE_FILE), { sourcemaps: SETTING.SASS.SOURCEMAPS })
-        .pipe(sassGlob())
-        .pipe(dartSass(SETTING.SASS.OPTION).on('error', dartSass.logError))
-        .pipe(autoprefixer(SETTING.SASS.AUTOPREFIXER))
-        .pipe(gulp.dest(SETTING.SASS.OUTPUT_PATH, { sourcemaps: './' }));
-    done();
+	gulp.src(path.join(SETTING.SASS.SOURCE_PATH, SETTING.SASS.SOURCE_FILE), { sourcemaps : SETTING.SASS.SOURCEMAPS })
+		.pipe(sassGlob())
+		.pipe(dartSass(SETTING.SASS.OPTION).on('error', dartSass.logError))
+		.pipe(autoprefixer(SETTING.SASS.AUTOPREFIXER))
+		.pipe(gulp.dest(SETTING.SASS.OUTPUT_PATH, { sourcemaps : './' }));
+	done();
 });
 
-gulp.task('webpack', (done) => {
-    SETTING.WEBPACK.OPTION.entry = {};
 
-    glob.sync(SETTING.WEBPACK.SOURCE_FILE, { ignore: SETTING.WEBPACK.IGNORE_FILE, cwd: SETTING.WEBPACK.SOURCE_PATH}).forEach(function(name) {
-        SETTING.WEBPACK.OPTION.entry[name] = './' + path.join(SETTING.WEBPACK.JS_SOURCE_PATH, name);
-    });
-
-    gulp.src(path.join(SETTING.WEBPACK.JS_SOURCE_PATH, SETTING.WEBPACK.SOURCE_FILE))
-        .pipe(plumber())
-        .pipe(webpackStream(SETTING.WEBPACK.OPTION, webpack))
-        .pipe(gulp.dest(SETTING.WEBPACK.OUTPUT_PATH));
-    done();
-});
 
 gulp.task('browsersync', (done) => {
-    browsersync.init({
-        server: {
-            baseDir: SETTING.BROWSERSYNC.DOCUMENT_ROOT,
-            index: 'index.html',
-        },
-        middleware: [
-            connectSSI({
-                baseDir: SETTING.BROWSERSYNC.DOCUMENT_ROOT,
-                ext: '.html'
-            })
-        ],
-        startPath: SETTING.BROWSERSYNC.STARTPATH,
-        ghostMode: SETTING.BROWSERSYNC.GHOSTMODE,
-        notify: SETTING.BROWSERSYNC.NOTIFY,
-    });
-    done();
+	browsersync.init({
+		server : {
+			baseDir : SETTING.BROWSERSYNC.DOCUMENT_ROOT,
+			index : 'index.html',
+		},
+		middleware : [
+			connectSSI({
+				baseDir : SETTING.BROWSERSYNC.DOCUMENT_ROOT,
+				ext : '.html'
+			})
+		],
+		startPath : SETTING.BROWSERSYNC.STARTPATH,
+		ghostMode : SETTING.BROWSERSYNC.GHOSTMODE,
+		notify : SETTING.BROWSERSYNC.NOTIFY,
+	});
+	done();
 });
 
+
 gulp.task('browsersync-connect-php', (done) => {
-    connectPHP.server({
-        port: SETTING.PHP_CONNECT.PORT,
-        base: SETTING.BROWSERSYNC.DOCUMENT_ROOT
-    }, () => {
-        browsersync.init({
-            proxy: 'localhost:' + SETTING.PHP_CONNECT.PORT,
-            middleware: [
-                connectSSI({
-                    baseDir: SETTING.BROWSERSYNC.DOCUMENT_ROOT,
-                    ext: '.html'
-                })
-            ],
-            startPath: SETTING.BROWSERSYNC.STARTPATH,
-            ghostMode: SETTING.BROWSERSYNC.GHOSTMODE,
-            notify: SETTING.BROWSERSYNC.NOTIFY,
-        })
-    });
-    done();
+	connectPHP.server({
+		port : SETTING.PHP_CONNECT.PORT,
+		base : SETTING.BROWSERSYNC.DOCUMENT_ROOT
+	}, () => {
+		browsersync.init({
+			proxy : 'localhost:' + SETTING.PHP_CONNECT.PORT,
+			middleware : [
+				connectSSI({
+					baseDir : SETTING.BROWSERSYNC.DOCUMENT_ROOT,
+					ext : '.html'
+				})
+			],
+			startPath : SETTING.BROWSERSYNC.STARTPATH,
+			ghostMode : SETTING.BROWSERSYNC.GHOSTMODE,
+			notify : SETTING.BROWSERSYNC.NOTIFY,
+		})
+	});
+	done();
 });
 
 gulp.task('browsersync-reload', (done) => {
-    browsersync.reload();
-    done();
+	browsersync.reload();
+	done();
 });
 
 gulp.task('watch', (done) => {
-    gulp.watch(SETTING.SASS.SOURCE_PATH + '/**/*.scss', gulp.series('sass'));
-    gulp.watch(SETTING.WEBPACK.JS_SOURCE_PATH + '/**/*.js', gulp.series('webpack'));
-    gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.php', gulp.series('browsersync-reload'));
-    gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.html', gulp.series('browsersync-reload'));
-    gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.css', gulp.series('browsersync-reload'));
-    gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.js', gulp.series('browsersync-reload'));
-    done();
+	gulp.watch(path.join(SETTING.SASS.SOURCE_PATH, SETTING.SASS.SOURCE_FILE), gulp.series('sass'));
+	gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.php' ,  gulp.series('browsersync-reload'));
+	gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.html',  gulp.series('browsersync-reload'));
+	gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.css' ,  gulp.series('browsersync-reload'));
+	gulp.watch(SETTING.BROWSERSYNC.DOCUMENT_ROOT + '/**/*.js'  ,  gulp.series('browsersync-reload'));
+	done();
 });
 
-gulp.task('default', gulp.series(
-    'sass',
-    'webpack',
-    'browsersync',
-    'watch'
-));
+gulp.task('default', (() => {
+	SETTING.SASS.SOURCEMAPS = true;
 
-gulp.task('php', gulp.series(
-    'sass',
-    'webpack',
-    'browsersync-connect-php',
-    'watch'
-));
+	return gulp.series(
+		'sass',
+		'browsersync',
+		'watch'
+	)
+})());
 
-gulp.task('prod', gulp.series(
-    'sass',
-    'webpack',
-    'browsersync',
-    'watch'
-));
+gulp.task('php', (() => {
+	SETTING.SASS.SOURCEMAPS = true;
+
+	return gulp.series(
+		'sass',
+		'browsersync-connect-php',
+		'watch'
+	)
+})());
+
+gulp.task('prod', (() => {
+	SETTING.SASS.SOURCEMAPS = false;
+
+	return gulp.series(
+		'sass',
+		'browsersync',
+		'watch'
+	)
+})());
--- gulpfile.setting.js
+++ gulpfile.setting.js
@@ -8,30 +8,6 @@ module.exports = {
 		},
 		AUTOPREFIXER : { cascade : false },
 	},
-	WEBPACK : {
-		SOURCE_PATH : 'source',
-		SOURCE_FILE : '**/*.js',
-		IGNORE_FILE : '**/_*.js',
-		OUTPUT_PATH : 'public',
-		OPTION : {
-			mode : 'development',
-			entry : {},
-			output : { filename : '[name]' },
-			module : {
-				rules: [ {
-					test : /\.js$/,
-					use : [
-						{
-							loader : 'babel-loader',
-							options : { presets : ['@babel/preset-env'] }
-						}
-					]
-				} ]
-			},
-			devtool : 'source-map',
-			target : ["web", "es5"],
-		}
-	},
 	BROWSERSYNC : {
 		DOCUMENT_ROOT : 'public',
 		STARTPATH : '',
